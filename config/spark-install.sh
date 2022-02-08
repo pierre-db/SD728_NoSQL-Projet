7163dcc5acb8700a6a857078f8ba2f9f761d079e
pierre-db <pierre.dalbianco@free.fr>
2022-02-08 14:32:52 +0100

    fixed command to launch spark

--- config/spark-install.sh
+++ config/spark-install.sh
@@ -32,4 +32,4 @@ echo "SPARK_MASTER_HOST=tp-hadoop-1" >> /opt/spark/conf/spark-env.sh
 # start-workers.sh 
 
 # pour lancer une shell pyspark
-# pyspark --deploy-mode client --master spark://tp-hadoop-1:7077 --conf spark.cassandra.connection.host=tp-hadoop-1 --packages com.datastax.spark:spark-cassandra-connector_2.12:3.1.0
+# pyspark --deploy-mode client --master spark://tp-hadoop-1:7077 --conf spark.cassandra.connection.host=tp-hadoop-1 --packages com.datastax.spark:spark-cassandra-connector_2.12:3.1.0 --conf spark.cassandra.input.consistency.level=ONE --conf spark.cassandra.output.consistency.level=ONE
