61b661f6000433f9d234c22a7b9be7ba23a99152
pierre-db <pierre.dalbianco@free.fr>
2022-02-09 15:00:10 +0100

    fixing the SQL/CQL requests

--- scripts/requests.sql
+++ scripts/requests.sql
@@ -20,62 +20,63 @@ CREATE KEYSPACE production WITH replication = {'class': 'SimpleStrategy', 'repli
 
 -- table_ab
 CREATE TABLE table_ab (
-    event_id bigint,
-    mention_id bigint,
     pays text,
+    event_id int,
+    annee_event int,
+    mois_event int,
+    jour_event int,
+    annee_mention int,
+    mois_mention int,
+    jour_mention int,
     langue text,
-    jour bigint,
-    mois int,
-    annee int,
-    total int,
-    PRIMARY KEY ((pays), event_id, jour, mois, annee)
+    total bigint,
+    PRIMARY KEY ((pays), event_id, annee_event, mois_event, jour_event, annee_mention,  mois_mention, jour_mention)
 );
 
 -- table_c
-CREATE TABLE table_cd (
+CREATE TABLE table_c (
     source text,
     theme text,
     personne text,
     lieu text,
-    total int,
-    somme_ton float,
-    jour bigint,
-    mois int,
     annee int,
-    PRIMARY KEY ((source), theme, personne, lieu, jour, mois, annee)
+    mois int,
+    jour int,
+    somme_ton double,
+    total bigint,
+    PRIMARY KEY ((source), theme, personne, lieu, annee, mois, jour)
 );
 
 -- table_d
 CREATE TABLE table_d (
     lieu text,
     langue text,
-    total int,
-    somme_ton float,
-    jour bigint,
-    mois int,
     annee int,
-    PRIMARY KEY ((lieu, langue), jour, mois, annee)
+    mois int,
+    jour int,
+    somme_ton double,
+    total bigint,
+    PRIMARY KEY ((lieu, langue), annee, mois, jour)
 );
 
 -- Requêtes CQL de réponse aux questions
 -- a
-SELECT * FROM table_ab;
+SELECT pays, jour_mention, langue, total FROM table_ab WHERE pays='FR';
 
 -- b
-SELECT event_id, SUM(total) as compte, annee --, mois, jour
+SELECT event_id, SUM(total) as compte
 FROM table_ab
 WHERE pays = 'input'
-GROUP BY annee -- mois, jour
-ORDER BY compte DESC;
+GROUP BY event_id, annee_event, mois_event, jour_event;
 
 -- c
 SELECT source, theme, personne, lieu, SUM(total) AS somme_total, SUM(somme_ton) AS somme_ton, jour -- mois, annee
 FROM table_c
 WHERE source = 'input'
-GROUP BY theme, personne, lieu, annee; -- mois, jour
+GROUP BY theme, personne, lieu, annee_event; -- mois, jour
 
 -- d
-SELECT langue, lieu, SUM(total) AS somme_total, SUM(somme_ton) AS somme_ton, jour
+SELECT langue, lieu, SUM(total) AS somme_total, SUM(somme_ton) AS somme_ton
 FROM table_d
 WHERE langue = 'input1' AND lieu = 'input2'
-GROUP BY annee; -- mois, jour
+GROUP BY annee_event; -- mois, jour
